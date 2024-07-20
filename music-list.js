8137b83c3a8944d8d129d8189c6488e5aac83997
Tushar Kumar <knightbros889@gmail.com>
2024-07-20 23:34:48 +0530

    Files

---
+++ music-list.js
@@ -0,0 +1,63 @@
+
+let allMusic = [
+    {
+        name: "Heroine",
+        artist: "Neel Kamal Singh",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Jale-2",
+        artist: " Sapna Choudhary, Shiva Chaudhary",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Let Me Down Slowly",
+        artist: "Alec Benjamin",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Bam Lehri",
+        artist: "RhymSter",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Patakha Guddi",
+        artist: "Nooran Sisters",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Girls Like You",
+        artist: "Maroon 5",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "On My Way",
+        artist: "Alan Walker",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Sorry",
+        artist: "Justin Beiber",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Nakkadwaley Disco, Udhaarwaley Khisko",
+        artist: "Ram Sampath",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Me,Myself & I",
+        artist: "G-Eazy",
+        img: "Me-Myself-I",
+        src: "music-10"
+    },
+];
